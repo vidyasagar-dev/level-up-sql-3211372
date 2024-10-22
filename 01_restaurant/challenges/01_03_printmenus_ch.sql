10f683c570cddd362ab8b673718c1744e88d8423
vidyasagar-dev <vidyasagar.sama@gmail.com>
2024-10-22 11:40:56 +0000

    Pending changes exported from your codespace

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,9 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+Select * from Dishes ORDER BY price
+
+Select * from Dishes where Type in('appetizer', 'beverage')
+
+Select * from Dishes where Type != 'beverage'
--- 01_restaurant/challenges/01_04_newcustomer_ch.sql
+++ 01_restaurant/challenges/01_04_newcustomer_ch.sql
@@ -4,3 +4,7 @@
 -- Anna Smith (asmith@samoca.org)
 -- 479 Lapis Dr., Memphis, TN
 -- Phone: (555) 555-1212; Birthday: July 21, 1973
+
+INSERT INTO Customer(FirstName, LastName, Email, Address, City, State, Phone, Birthday)
+VALUES('Anna', 'Smith', 'asmith@samoca.org', '479 Lapis Dr.', 'Memphis', 'TN', '(555) 555-1212', '1973-07-21');
+
